9e26480a392c431e7dbcef86ebd6c4ba8a3602fb
Emma Thompson <emma.thompson@oit.edu>
2017-01-27 13:51:05 -0800

    3/3 tests passing

--- cSymbolTable.h
+++ cSymbolTable.h
@@ -13,30 +13,81 @@
 #include <unordered_map>
 #include <list>
 
+using std::list;
+using std::unordered_map;
+using std::string;
+
 class cSymbolTable{
     public:
      void DecreaseScope() 
      {
-        
+        if (!cSymbolList.empty()) 
+        {
+            cSymbolList.pop_front();
+        }
      }
+  
+     //************************************
      void IncreaseScope()
      {
-     
+         unordered_map<string, cSymbol *> hashtable;
+         cSymbolList.push_front(hashtable);
      }
-     cSymbol * Insert(std::string yytxt) 
+  
+     //************************************
+     cSymbol * Insert(string yytxt) 
      {
-         return NULL;
-     }   
-     cSymbol * LookUpAll(std::string) 
-     {
-         return NULL;
-     }
-     cSymbol * LookUpRecent(std::string) 
-     {
-         return NULL;
+         unordered_map<string, cSymbol *>::iterator iter = 
+            cSymbolList.front().find(yytxt);
+         cSymbol * ret_val = nullptr;
+
+
+         if(iter != cSymbolList.front().end())
+         {
+             ret_val = iter->second;
+         }
+         else
+         {
+             ret_val = new cSymbol(yytxt);
+             cSymbolList.front().insert({yytxt, ret_val});
+         }
+
+         return ret_val;
      }
 
-    private:
-     std::list <std::unordered_map<std::string, cSymbol *>> cSymbolList;
+     //************************************
+     cSymbol * LookUpAll(string yytxt) 
+     {
+         cSymbol * ret_val = nullptr;
+
+         for(auto iter = cSymbolList.begin(); iter != cSymbolList.end(); ++iter)
+         {
+             unordered_map<string, cSymbol *>::iterator iter_2 = 
+                 iter->find(yytxt);
+             if(iter_2 != iter->end())
+             {
+                 ret_val = iter_2->second;
+             }
+         }
+         return ret_val;
+     }
+
+     //************************************
+     cSymbol * LookUpRecent(string yytxt) 
+     {
+          unordered_map<string, cSymbol *>::iterator iter =
+             cSymbolList.front().find(yytxt);
+          cSymbol * ret_val = nullptr;
+          
+          if(iter != cSymbolList.front().end())
+          {
+             ret_val = iter->second;
+          }
+         
+          return ret_val;
+     }
+
+    protected:
+     list <unordered_map<string, cSymbol *>> cSymbolList;
 
 };
