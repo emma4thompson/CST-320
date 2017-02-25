7079b7fa66d03199b221091a3b7ecc8732d75e76
Emma Thompson <emma.thompson@oit.edu>
2017-02-25 13:45:59 -0800

    DONE. 7/7 tests passing.

--- cFuncDeclNode.h
+++ cFuncDeclNode.h
@@ -40,7 +40,8 @@ class cFuncDeclNode : public cDeclNode
             }
             else
             {
-                if(!IsFunc())
+                //if localName isn't a function
+                if(!(localName->GetDecl()->IsFunc()))
                 {
                     SemanticError(name->GetName() + string(" previously declared as other than a function"));
                     //adding children because we're creating a new function 
@@ -51,24 +52,27 @@ class cFuncDeclNode : public cDeclNode
                     AddChild(nullptr);
                     AddChild(nullptr);
                 }
-
-                //old_decl is a function
-                cFuncDeclNode * old_decl = dynamic_cast<cFuncDeclNode*>(localName->GetDecl());
-
-                if(old_decl->GetReturnType()->GetDecl() != type->GetDecl())
-                {
-                    SemanticError(name->GetName() + string(" previously declared with different return type"));
-                    AddChild(type);
-                    AddChild(name);
-                    AddChild(nullptr);
-                    AddChild(nullptr);
-                    AddChild(nullptr);
-                }
                 else
                 {
-                    name->SetDecl(this);
-                    m_children = old_decl->m_children;
+                    //old_decl is a function
+                    cFuncDeclNode * old_decl = dynamic_cast<cFuncDeclNode*>(localName->GetDecl());
+
+                    if(old_decl->GetReturnType()->GetDecl() != type->GetDecl())
+                    {
+                        SemanticError(name->GetName() + string(" previously declared with different return type"));
+                        AddChild(type);
+                        AddChild(name);
+                        AddChild(nullptr);
+                        AddChild(nullptr);
+                        AddChild(nullptr);
+                    }
+                    else
+                    {
+                        name->SetDecl(this);
+                        m_children = old_decl->m_children;
+                    }
                 }
+                SetHasParams();
             }
 //            AddChild(type);
 //            AddChild(name);
@@ -90,6 +94,35 @@ class cFuncDeclNode : public cDeclNode
 
         void AddParams(cParamsNode *params)
         {
+            if(GetHasParams())
+            {
+                if(this->GetLocalParams() != nullptr && params != nullptr)
+                {
+                   if(params->GetNumParams() != this->GetLocalParams()->GetNumParams())
+                   {
+                        SemanticError(this->GetName()->GetName() + string(" previously declared with a different number of parameters"));
+                   }
+                   else
+                   {
+                        bool hasError;
+                        for(int i = 0; i < params->GetNumParams(); i++)
+                        {
+                            if(params->GetParam(i)->GetType() != this->GetLocalParams()->GetParam(i)->GetType())
+                            {
+                                hasError = true;
+                            }
+                        }   
+                        if(hasError)
+                        {
+                            SemanticError(this->GetName()->GetName() + string(" previously declared with different parameters"));
+                        }
+                   } 
+                }
+                else if (this->GetLocalParams() != nullptr || params != nullptr)
+                {
+                     SemanticError(this->GetName()->GetName() + string(" previously declared with a different number of parameters"));
+                }
+            }
             SetChild(2, params);
         }
 
@@ -100,7 +133,22 @@ class cFuncDeclNode : public cDeclNode
 
         void AddStmts(cStmtsNode *stmts)
         {
+            if(GetStmts() != nullptr)
+            {
+                SemanticError(GetName()->GetName() + string(" already has a definition"));
+            }
+
             SetChild(4, stmts);
+        }
+
+        bool GetHasParams()
+        {
+            return hasParams;        
+        }
+
+        void SetHasParams()
+        {
+            hasParams = true;
         }
 
         virtual cSymbol *GetName()
@@ -118,10 +166,25 @@ class cFuncDeclNode : public cDeclNode
             return static_cast<cSymbol*>(GetChild(0));
         }
 
+        cParamsNode * GetLocalParams()
+        {
+            return static_cast<cParamsNode*>(GetChild(2));
+        }
+        
+        cStmtNode * GetStmts()
+        {
+            return static_cast<cStmtNode*>(GetChild(4));
+        }
+
         virtual bool IsFunc()  {return true;}
 
         virtual string NodeType() { return string("func"); }
         virtual void Visit(cVisitor *visitor) { visitor->Visit(this); }
+
+
+    private:
+        bool hasParams;
+
 };
 
     
--- cParamsNode.h
+++ cParamsNode.h
@@ -23,6 +23,16 @@ class cParamsNode : public cDeclsNode
             AddChild(multiple);
         }
 
+        int GetNumParams()
+        {
+            return NumChildren();
+        }
+
+        cDeclNode * GetParam(int index)
+        {
+            return static_cast<cDeclNode *>(GetChild(index));
+        }
+
         virtual string NodeType() { return string("args"); }
         virtual void Visit(cVisitor *visitor) { visitor->Visit(this); }
 };
